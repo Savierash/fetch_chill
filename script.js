12fd4c198d362bf85bbaf27db2bad84158e0d66d
Suniga, Richelle P. <dump1473@gmail.com>
2025-02-11 15:49:22 +0800

    new login

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
-function toggleForm() {
-            let loginForm = document.getElementById("loginForm");
-            let signupForm = document.getElementById("signupForm");
-            
-            if (loginForm.style.display === "none") {
-                loginForm.style.display = "block";
-                signupForm.style.display = "none";
-            } else {
-                loginForm.style.display = "none";
-                signupForm.style.display = "block";
-            }
-        }
+
+const sign_in_btn = document.querySelector("#sign-in-btn");
+const sign_up_btn = document.querySelector("#sign-up-btn");
+const container = document.querySelector(".container");
+
+sign_up_btn.addEventListener("click", () => {
+  container.classList.add("sign-up-mode");
+});
+
+sign_in_btn.addEventListener("click", () => {
+  container.classList.remove("sign-up-mode");
+});
