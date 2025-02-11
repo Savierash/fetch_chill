f0b25a1c96878935126fe05be95bfe7c07fde5b9
Suniga, Richelle P. <dump1473@gmail.com>
2025-02-11 17:50:45 +0800

    add home ,about style

--- home.js
+++ home.js
@@ -1,5 +1,5 @@
 //sign up requird fields
-if (sessionStorage.getItem("isLoggedIn") !== "true") {
-    window.location.href = "index.html"; // Babalik sa login page kung hindi naka-login
-}
+//if (sessionStorage.getItem("isLoggedIn") !== "true") {
+   // window.location.href = "index.html"; // Babalik sa login page kung hindi naka-login
+//}
 
